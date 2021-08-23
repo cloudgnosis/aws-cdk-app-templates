c1417fb0640f368577186574fae6f234de868dc2
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2021-08-23 08:26:55 +0200

    build: add keywords and description

--- .projenrc.js
+++ .projenrc.js
@@ -3,21 +3,18 @@ const project = new JsiiProject({
   author: 'Erik Lundevall Zara',
   authorAddress: '45368867+eriklz@users.noreply.github.com',
   defaultReleaseBranch: 'main',
-  name: 'cloudgnosis-aws-cdk-app-templates',
+  name: '@cloudgnosis/aws-cdk-app-templates',
   repositoryUrl: 'git@github.com:cloudgnosis/aws-cdk-app-templates.git',
-  peerDeps: [
-    'projen',
-  ],
   minNodeVersion: '14.15.0',
   gitignore: [
     '.idea/',
   ],
-
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // release: undefined,                /* Add release management to this project. */
+  keywords: [
+    'aws',
+    'cdk',
+    'projen',
+    'typescript',
+  ],
+  description: 'Collection of project types for setting up AWS CDK-based apps with Typescript',
 });
 project.synth();
